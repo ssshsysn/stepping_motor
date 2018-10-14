cb0bbc0c7152f391b708157bdaf0bff4b4eb0d32
ssshsysn <ssshsysn@gmail.com>
2018-10-14 14:51:18 +0900

    Add motor's Status and Direction

--- Src/mycode/stepping_motor.c
+++ Src/mycode/stepping_motor.c
@@ -1,6 +1,21 @@
 #include "stm32f4xx_hal.h"
 #include "stepping_motor.h"
 
+// Motor Status
+typedef enum {
+    MTS_IDLE        = 0,    // IDLE
+    MTS_RUN_ACCEL,          // RUNNING ACCEL(Slow Up)
+    MTS_RUN_CONST,          // RUNNING CONSTANT
+    MTS_RUN_DECEL,          // RUNNING DECEL(Slow Down)
+    MTS_BREAK,              // BREAKING(Have Timeout)
+}MOTOR_STATUS;
+
+// Motor Direction
+typedef enum {
+    MTD_CW      = 0,
+    MTD_CCW,
+}MOTOR_DIRECTION;
+
 // Phase
 #define PHASE_A1    (0)
 #define PHASE_B1    (1)
@@ -30,8 +45,8 @@ static const GPIO_PinState sc_OutputState[MOTOR_OFF_INDEX+1][PHASE_MAX] = {
 
 // Phase mode
 typedef enum {
-    PHASE_2     = 0,    // 2Phase mode
-    PHASE_1_2,          // 1-2Phase mode
+    MTP_PHASE_2     = 0,    // 2Phase mode
+    MTP_PHASE_1_2,          // 1-2Phase mode
 }PHASE_MODE;
 
 // Motor pin information structure
@@ -43,6 +58,8 @@ typedef struct {
 
 // Motor information structure
 typedef struct {
+    MOTOR_STATUS        status;                 // motor status
+    MOTOR_DIRECTION     direction;              // motor direction
     PHASE_MODE          phase_mode;             // phase mode
     uint32_t            phase_index;            // phase current index
     MOTOR_PIN_INFO      phase[PHASE_MAX];       // phase information
@@ -53,7 +70,9 @@ typedef struct {
 #define MOTOR_MAX       (1)             // the number of motors  
 static MOTOR_INFO       motors[MOTOR_MAX] = {
     {   // Motor0 information
-        PHASE_2,        // phase mode
+        MTS_IDLE,       // motor status
+        MTD_CW,         // motor cw
+        MTP_PHASE_2,    // phase mode
         MOTOR_OFF_INDEX,// phase current index
         {               // phase(pin) information
             {GPIOA, GPIO_PIN_10, GPIO_PIN_RESET},    // A1
@@ -67,32 +86,43 @@ static MOTOR_INFO       motors[MOTOR_MAX] = {
 
 // Private functions definition 
 static uint32_t MotorUpdate( MOTOR_INFO* const pMtr );
+static void MotorUpdatePhase( MOTOR_INFO* const pMtr );
 static void MotorSetup( MOTOR_INFO* const pMtr );
 static void MotorOutput( const MOTOR_INFO* const pMtr );
 
 // function : Update for Motor information
 static uint32_t MotorUpdate( MOTOR_INFO* const pMtr )
 {
-    if( pMtr->phase_mode == PHASE_2 ){
-        // 2Phase mode
-        pMtr->phase_index += 2;
-    }
-    else{
-        // 1-2Phase mode
-        pMtr->phase_index += 1;
-    }
-    pMtr->phase_index &= MOTOR_PHASE_MASK;
+    // Check Status
+    if( pMtr->status == MTS_IDLE )  return 0;
+    // Update Phase Index
+    MotorUpdatePhase( pMtr );
+    // Update Current Position
     pMtr->pos++;
 
     return 1;
 }
 
+// function : Update for Phase Index
+static void MotorUpdatePhase( MOTOR_INFO* const pMtr )
+{
+    // Phase Index
+    int32_t direction_update;
+    // Phase mode
+    if( pMtr->phase_mode == PHASE_2 )   direction_update = 2;   // 2Phase mode
+    else                                direction_update = 1;   // 1-2Phase mode
+    // Direction
+    if( pMtr->direction == MTD_CCW )    direction_update *= -1;
+    // Update
+    pMtr->phase_index += direction_update;
+    pMtr->phase_index &= MOTOR_PHASE_MASK;
+}
+
 // function : Set up for Motor output status
 static void MotorSetup( MOTOR_INFO* const pMtr )
 {
-    if( pMtr->phase_index > MOTOR_OFF_INDEX ){
-        return;
-    }
+    // check pahse index range
+    if( pMtr->phase_index > MOTOR_OFF_INDEX )   return;
 
     uint16_t nIndex = pMtr->phase_index;
     MOTOR_PIN_INFO* pInfo = &(pMtr->phase[0]);
@@ -117,7 +147,9 @@ void MotorInitialize( void )
 {
     MOTOR_INFO* pMtr;
     for(uint16_t nMotor=0; nMotor < MOTOR_MAX; nMotor++ ){
-        motors[nMotor].phase_mode    = PHASE_1_2;
+        motors[nMotor].status        = MTS_IDLE;
+        motors[nMotor].direction     = MTD_CW;
+        motors[nMotor].phase_mode    = MTP_PHASE_2;
         motors[nMotor].phase_index   = MOTOR_OFF_INDEX;
         motors[nMotor].pos           = 0; 
         pMtr = &(motors[nMotor]);
