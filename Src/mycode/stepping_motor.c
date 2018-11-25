dec61bc8040b06ca512c677379d81982ae83d9b4
ssshsysn <ssshsysn@gmail.com>
2018-11-25 19:34:38 +0900

    Add PPS Control Proccess And Modify

--- Src/mycode/stepping_motor.c
+++ Src/mycode/stepping_motor.c
@@ -1,6 +1,12 @@
 #include "stm32f4xx_hal.h"
 #include "stepping_motor.h"
 
+// Interrupt Timer interval
+#define INTERRUPT_TIMER_INTERVAL  (1000)    // 1000ms / Interrupt interval(ms)
+
+// default value for PPS
+#define DEFAULT_PPS               (1000)
+
 // Motor Status
 typedef enum {
     MTS_IDLE        = 0,    // IDLE
@@ -60,12 +66,15 @@ typedef struct {
 typedef struct {
     MOTOR_STATUS        status;                 // motor status
     MOTOR_DIRECTION     direction;              // motor direction
+    uint32_t            pps;                    // PPS
+    uint32_t            pps_timer;              // PPS count down timer for phase output
     PHASE_MODE          phase_mode;             // phase mode
     int32_t             phase_index_update_num; // phase index update number
     uint32_t            phase_index;            // phase current index
     uint32_t            phase_pos;              // phase current position
     MOTOR_PIN_INFO      phase[PHASE_MAX];       // phase information
-    uint32_t            break_timeout;          // timeout for motor failure mode
+    uint32_t            break_timeout;          // breaking timeout
+    uint32_t            break_timer;            // count down for breaking timeout 
     int32_t             motor_position;         // motor position
     int32_t             target_position;        // target position
 }MOTOR_INFO;
@@ -76,6 +85,8 @@ static MOTOR_INFO       motors[MOTOR_MAX] = {
     {   // Motor0 information
         MTS_IDLE,       // motor status
         MTD_CW,         // motor cw
+        DEFAULT_PPS,    // PPS
+        (uint32_t)(INTERRUPT_TIMER_INTERVAL/DEFAULT_PPS),      // PPS count down timer for phase output
         MTP_PHASE_FULL, // phase mode
         2,              // phase index update number
         MOTOR_OFF_INDEX,// phase current index
@@ -86,37 +97,40 @@ static MOTOR_INFO       motors[MOTOR_MAX] = {
             {GPIOA, GPIO_PIN_8,  GPIO_PIN_RESET},    // A2
             {GPIOA, GPIO_PIN_9,  GPIO_PIN_RESET},    // B2
         },
-        0,              // breaking timeout
+        10,             // breaking timeout
+        0,              // count down for breaking timeout 
         0,              // motor position
         0,              // target position
     },
 };
 
 // Private functions definition 
-static uint32_t MotorUpdate( MOTOR_INFO* const pMtr );
+static void MotorUpdate( MOTOR_INFO* const pMtr );
 static void MotorUpdateNextStatus( MOTOR_INFO* const pMtr );
-static void MotorUpdatePhase( MOTOR_INFO* const pMtr );
+static uint32_t MotorUpdatePhase( MOTOR_INFO* const pMtr );
+static uint32_t MotorUpdatePhaseIfConst( MOTOR_INFO* const pMtr );
 static uint32_t MotorUpdatePhaseIfBreak( MOTOR_INFO* const pMtr );
 static void MotorUpdateCurrentPosition( MOTOR_INFO* const pMtr );
-static void MotorUpdateBreakingTimeout( MOTOR_INFO* const pMtr );
+static void MotorUpdatePPSTimer( MOTOR_INFO* const pMtr );
 static void MotorDecisionPhaseIndexUpdateNumber( MOTOR_INFO* const pMtr );
 static void MotorSetup( MOTOR_INFO* const pMtr );
 static void MotorOutput( const MOTOR_INFO* const pMtr );
 
 // function : Update for Motor information
-static uint32_t MotorUpdate( MOTOR_INFO* const pMtr )
+static void MotorUpdate( MOTOR_INFO* const pMtr )
 {
     // Check Status
-    if( pMtr->status == MTS_IDLE )  return 0;
-    // Update Breaking Timeout
-    MotorUpdateBreakingTimeout( pMtr );
-    // Update Phase Index
-    MotorUpdatePhase( pMtr );
-    // Update Current Motor position
-    MotorUpdateCurrentPosition( pMtr );
+    if( pMtr->status == MTS_IDLE )  return;
+    // Update Phase
+    if( MotorUpdatePhase( pMtr ) == 1 ){
+        MotorSetup( pMtr );
+        MotorOutput( pMtr );
+        MotorUpdateCurrentPosition( pMtr );
+    }
+    // Update PPS Timer
+    MotorUpdatePPSTimer( pMtr );
     // Update status for next process
     MotorUpdateNextStatus( pMtr );
-    return 1;
 }
 
 // function : Update for motor Next status
@@ -132,54 +146,79 @@ static void MotorUpdateNextStatus( MOTOR_INFO* const pMtr )
             if( pMtr->target_position == pMtr->motor_position ) pMtr->status = MTS_BREAK;
             break;
         case MTS_BREAK:
-            if( pMtr->break_timeout == 0 ) pMtr->status = MTS_IDLE;
+            if( pMtr->break_timer == 0 ) pMtr->status = MTS_IDLE;
             break;
     }
 }
 
 // function : Update for Phase Index
-static void MotorUpdatePhase( MOTOR_INFO* const pMtr )
+static uint32_t MotorUpdatePhase( MOTOR_INFO* const pMtr )
 {
     // Phase Index
+    // Check Constant stepping
+    if( MotorUpdatePhaseIfConst(pMtr) == 1 ) return 1;
     // Check Breaking timeout
-    if( MotorUpdatePhaseIfBreak(pMtr) == 0 ) return;
+    if( MotorUpdatePhaseIfBreak(pMtr) == 1 ) return 1;
+
+    return 0;
+}
+static uint32_t MotorUpdatePhaseIfConst( MOTOR_INFO* const pMtr )
+{
+    if( pMtr->status != MTS_RUN_CONST )  return 0;
+    if( pMtr->pps_timer != ((uint32_t)(INTERRUPT_TIMER_INTERVAL / pMtr->pps)) ) return 0;
 
     // Update
     pMtr->phase_index += pMtr->phase_index_update_num;
     pMtr->phase_index &= MOTOR_PHASE_MASK;
+    return 1;
 }
+
 static uint32_t MotorUpdatePhaseIfBreak( MOTOR_INFO* const pMtr )
 {
     if( pMtr->status != MTS_BREAK )  return 0;
- 
+    
     // Check Breaking timeout
-    // break_timeout = 0 then output off and change status to IDLE
-    // break_timeout > 0 then output keep
-    if( pMtr->break_timeout == 0 ){
+    if( pMtr->break_timer > 0 ){
+        pMtr->break_timer--;
+    }
+ 
+    // break_timer = 0 then output off and change status to IDLE
+    // break_timer > 0 then output keep
+    if( pMtr->break_timer == 0 ){
         pMtr->phase_pos     = pMtr->phase_index;
         pMtr->phase_index   = MOTOR_OFF_INDEX;
+        return 1;
     }
-    return 1;
+    else{
+        return 0;
+    }
 }
 
 // function : Update for Current Position
 static void MotorUpdateCurrentPosition( MOTOR_INFO* const pMtr )
 {
-    if( pMtr->status == MTS_BREAK || pMtr->status == MTS_IDLE ) return;
+    //if( pMtr->status == MTS_BREAK || pMtr->status == MTS_IDLE ) return;
     if( pMtr->phase_mode == MTP_PHASE_HALF ){
         // now HALF-STEP position then return
         if( pMtr->phase_pos%2 ) return;
     }
+
     // Update
     if( pMtr->direction == MTD_CW ) pMtr->motor_position++;
     else                            pMtr->motor_position--;
 }
 
-// function : Update for Breaking Timeout
-static void MotorUpdateBreakingTimeout( MOTOR_INFO* const pMtr )
+// function : Update for PPS Timer
+static void MotorUpdatePPSTimer( MOTOR_INFO* const pMtr )
 {
-    if( pMtr->status != MTS_BREAK ) return;
-    if( pMtr->break_timeout > 0 )   pMtr->break_timeout--;
+    if( pMtr->status != MTS_RUN_CONST )  return;
+
+    // Check Phase Update time
+    pMtr->pps_timer--;
+    if( pMtr->pps_timer == 0 ){
+        // Reset pps_timer
+        pMtr->pps_timer = (uint32_t)(INTERRUPT_TIMER_INTERVAL / pMtr->pps);
+    }
 }
 
 // function : Desision Phase Index Update Number
@@ -223,11 +262,14 @@ void MotorInitialize( void )
     for(uint16_t nMotor=0; nMotor < MOTOR_MAX; nMotor++ ){
         motors[nMotor].status        = MTS_IDLE;
         motors[nMotor].direction     = MTD_CW;
+        motors[nMotor].pps           = DEFAULT_PPS;
+        motors[nMotor].pps_timer     = (uint32_t)(INTERRUPT_TIMER_INTERVAL/DEFAULT_PPS);
         motors[nMotor].phase_mode    = MTP_PHASE_FULL;
         motors[nMotor].phase_index_update_num = 2;
         motors[nMotor].phase_index   = MOTOR_OFF_INDEX;
         motors[nMotor].phase_pos     = 0;
-        motors[nMotor].break_timeout = 0,
+        motors[nMotor].break_timeout = 10,
+        motors[nMotor].break_timer = 0,
         motors[nMotor].motor_position   = 0; 
         motors[nMotor].target_position  = 0; 
         pMtr = &(motors[nMotor]);
@@ -241,30 +283,51 @@ void MotorInitialize( void )
         MotorSetup( pMtr );
         MotorOutput( pMtr );
         // TEST
-        motors[nMotor].status        = MTS_RUN_CONST;
-        motors[nMotor].direction     = MTD_CCW;
-        motors[nMotor].phase_index   = 0;
-        motors[nMotor].break_timeout = 6;
-        motors[nMotor].target_position = -12;
-        MotorDecisionPhaseIndexUpdateNumber(pMtr);   
+        //motors[nMotor].phase_pos     = 6;
+        //motors[nMotor].motor_position= -1; 
+        //MotorMove( 0, 1000, 12 );
+        //MotorMove( 0, 1000, -12 );
+        //MotorMove( 0, 1000, 0 );
+        //MotorMove( 0, 500, 4 );
+        //MotorMove( 0, 500, -4 );
     }
 }
 
 // function : Control for Motor output status
 void MotorControl( void )
 {
-    MOTOR_INFO* pMtr;
     for(uint16_t nMotor=0; nMotor < MOTOR_MAX; nMotor++ ){
-        pMtr = &(motors[nMotor]);
-        if( 0 !=  MotorUpdate( pMtr ) ){
-            MotorSetup( pMtr );
-            MotorOutput( pMtr );
-        }
+        MotorUpdate( &(motors[nMotor]) );
     }
 }
 
-// function : Toggle LED for test
-void ToggleLed( void )
+// function : Setup for Moving
+void MotorMove( uint16_t nMotor, uint32_t pps, int32_t position )
 {
-    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_10);
+    if( nMotor > (MOTOR_MAX - 1) )          return; 
+    if( pps == 0 )                          return; 
+    if( pps >  INTERRUPT_TIMER_INTERVAL )   return; 
+
+    // Disable Interrupt
+
+    // PPS Setup
+    motors[nMotor].pps          = pps;
+    motors[nMotor].pps_timer    = (uint32_t)(INTERRUPT_TIMER_INTERVAL / pps);
+
+    // Direction and target position Setup
+    if( position >= motors[nMotor].motor_position ) motors[nMotor].direction     = MTD_CW;
+    else                                            motors[nMotor].direction     = MTD_CCW;
+    motors[nMotor].target_position = position;
+
+    // Phase Setup
+    MotorDecisionPhaseIndexUpdateNumber( &motors[nMotor] );  
+
+    // Start
+    motors[nMotor].phase_index  = motors[nMotor].phase_pos;
+    motors[nMotor].break_timer  = motors[nMotor].break_timeout;
+    if( position == motors[nMotor].motor_position ) motors[nMotor].status = MTS_BREAK;
+    else                                            motors[nMotor].status = MTS_RUN_CONST;
+
+    // Enable Interrupt
+
 }
